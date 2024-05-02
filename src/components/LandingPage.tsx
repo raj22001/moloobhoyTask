d93cf4e1fe191876f1f553d8eb6694054b84472a
raj22001 <rajmahajan960@gmail.com>
2024-05-02 18:31:01 +0530

    code push

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,78 +1,95 @@
-
-import "./landingpage.css"
-import ParticleImage, { ParticleOptions , forces,
-  ParticleForce } from "react-particle-image";
-import logoImage from '../assets/logo4.png';
-import { useState } from "react";
+import "./landingpage.css";
+import ParticleImage, {
+  ParticleOptions,
+  forces,
+  ParticleForce,
+} from "react-particle-image";
+import logoImage from "../assets/logo4.png";
+import { useEffect, useState } from "react";
 import logo2 from "../assets/logo2.png";
 import logo3 from "../assets/logo3.png";
 import logo4 from "../assets/logo4.png";
+import LogoAll from "../assets/logoAll.png";
+import { Link } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import { IoVolumeMuteSharp } from "react-icons/io5";
+import { VscUnmute } from "react-icons/vsc";
+import AudioFile from "../assets/waveSound.mp3";
 
 const LandingPage = () => {
-
   const [hoveredDiv, setHoveredDiv] = useState(null);
+  const [isMuted, setIsMuted] = useState(false);
+
+  const [audio] = useState(new Audio(AudioFile));
+
+  useEffect(() => {
+    audio.play();
+  }, [audio]);
+
+  const toggleMute = () => {
+    if (audio) {
+      audio.muted = !audio.muted;
+      setIsMuted(audio.muted);
+    }
+  };
 
   const getTextContent = () => {
     switch (hoveredDiv) {
       case 1:
-        return "Text for A.S Moloobhoy & Sons Pvt Ltd";
+        return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
       case 2:
-        return "Text for About";
+        return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
       case 3:
-        return "Text for EMME";
+        return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
       case 4:
-        return "Text for A.S Moloobhoy & Sons Pvt Ltd";
+        return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
       case 5:
-        return "Text for Molobhoy Marine Services LLC";
+        return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
+      case 6:
+        return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
+      case 7:
+        return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
     }
   };
 
-  const buttonData = [
-    {
-      name: "A.S Moloobhoy & Sons Pvt Ltd",
-      text: "The Paragraphs module in Drupal provides editors with a component driven architecture for building pages. Morpht has been developing Paragraph approaches to site building since 2015. This case study captures some of the content from a Drupal 7 Paragraphs Demo site which was built to showcase what could be done with Paragraphs.",
-      url: "https://www.asmoloobhoy.com/",
-    },
-    {
-      name: "A.S Moloobhoy Pvt Ltd",
-      text: "Hi, Im a Demo Text of &nbsp;<a href='example.com'> A.S Moloobhoy Pvt Ltd</a>",
-    },
-    {
-      name: "Molobhoy Marine Services LLC",
-      text: "Hi, Im a Demo Text of A.S Moloobhoy FZCO",
-    },
-    {
-      name: "A.S Moloobhoy FZCO ",
-      text: "Hi, Im a Demo Text of Molobhoy Marine Services LLC",
-    },
-    {
-      name: "Moloobhoy Marine Equipment LLP",
-      text: "Hi, Im a Demo Text of Moloobhoy Marine Equipment LLP",
-    },
-    {
-      name: "Moloobhoy Marine Services WLL:Qatar",
-      text: "Hi, Im a Demo Text of Moloobhoy Marine Services WLL:Qatar",
-    },
-    {
-      name: "Moloobhoy Marine Services LLC-Oman",
-      text: "Hi, Im a Demo Text of Moloobhoy Marine Services LLC-Oman",
-    },
-  ];
-
-  const getLogoSource = () => {
+  const createLink = () => {
     switch (hoveredDiv) {
       case 1:
-        return logo4;
+        return (
+          <Link to="/moloobhoy">
+            Learn more about A.S Moloobhoy & Sons Pvt Ltd
+          </Link>
+        );
       case 2:
-        return logo2;
+        return <Link to="/about">About Us</Link>;
       case 3:
-        return logo3;
+        return <Link to="/emme">EMME</Link>;
       case 4:
-        return logo4;
+        return (
+          <Link to="/moloobhoy">
+            Learn more about A.S Moloobhoy & Sons Pvt Ltd
+          </Link>
+        );
       case 5:
-        return logo4;
+        return (
+          <Link to="/molobhoy">
+            Learn more about Molobhoy Marine Services LLC
+          </Link>
+        );
+      case 6:
+        return (
+          <Link to="/molobhoy">
+            Learn more about Molobhoy Marine Services LLC
+          </Link>
+        );
+      case 7:
+        return (
+          <Link to="/molobhoy">
+            Learn more about Molobhoy Marine Services LLC
+          </Link>
+        );
       default:
-        return null;
+        return "";
     }
   };
 
@@ -83,106 +100,146 @@ const LandingPage = () => {
       // Make a particle for this pixel if blue > 50 (range 0-255)
       return pixel.b > 50;
     },
-    color: ({ x, y, image }) => "#61dafb"
+    color: ({ x, y, image }) => "#ffffff",
   };
 
   const motionForce = (x: number, y: number): ParticleForce => {
     return forces.disturbance(x, y, 5);
   };
 
+  const resetHoveredDiv = () => {
+    setTimeout(() => {
+      setHoveredDiv(null);
+    }, 5000); // Change the delay (in milliseconds) as needed, e.g., 4000 for 4 seconds
+  };
+
+  // useEffect to call resetHoveredDiv when hoveredDiv changes
+  useEffect(() => {
+    if (hoveredDiv) {
+      resetHoveredDiv();
+    }
+  }, [hoveredDiv]);
 
   return (
-    <div className="w-full h-[100vh] flex justify-center items-center">
+    <div className="w-full h-[100vh] flex justify-center items-center relative">
+      <div className="left-[3%] top-[3%] absolute h-[60%]">
+        <div className="">
+          <img src={LogoAll} alt="" />
+        </div>
+        <div className="top-[90%] relative">
+          {isMuted ? (
+            <p onClick={toggleMute}>
+              <IoVolumeMuteSharp size={35} color="blue" />
+            </p>
+          ) : (
+            <p onClick={toggleMute}>
+              <VscUnmute size={35} color="blue" />
+            </p>
+          )}
+        </div>
+      </div>
       <div className="w-[50%] lg:h-[75%] relative flex ">
+        <ParticleImage
+          src={logoImage}
+          scale={0.3}
+          entropy={15}
+          maxParticles={4000}
+          particleOptions={particleOptions}
+          mouseMoveForce={motionForce}
+          touchMoveForce={motionForce}
+          className=" particle-image w-[75%] h-[95%] my-auto mx-auto flex justify-center items-center object-contain relative"
+          // backgroundColor="#ffffff"
+        />
 
-      <ParticleImage
-      src={logoImage}
-      scale={0.3}
-      entropy={15}
-      maxParticles={4200}
-      particleOptions={particleOptions}
-      mouseMoveForce={motionForce}
-      touchMoveForce={motionForce}
-      className="w-[75%] h-[95%] my-auto mx-auto flex justify-center items-center object-contain relative"
-      backgroundColor="#Ffffff"
-      />
+        {hoveredDiv && (
+          <div className="absolute top-[1%] left-0 w-full h-full flex justify-center items-center">
+            <div className="w-[70%] h-[95%] my-auto mx-auto bg-black rounded-full">
+              <div className="w-[100%] h-[90%] flex flex-col justify-between items-center]">
+                <p className="text-white  z-40 w-[90%] mx-auto h-[90%] flex justify-center items-center text-center">
+                  {getTextContent()}
+                </p>
+                <div className="w-full h-[10%] flex justify-center items-center">
 
-      {/* <div className="absolute lg:w-[75%] lg:h-[95%] my-auto xl:w-[90%] md:w-[100%] rounded-full bg-black z-40">
+                
+                <button className=" w-[40%] h-[100%]  z-40 text-white">
+                  <div className="left"></div>
+                  Hover me!
+                  <div className="right"></div>
+                </button>
+                
+                
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
 
-      </div> */}
-      <div className="absolute w-[90%] mx-auto h-[90%] m-auto ">
+        <div className="absolute w-[90%] mx-auto h-[90%] m-auto ">
           <div className="sm:hidden mobile:hidden md:block w-[100%] h-[100%] mx-auto ">
-            <div className=" flex h-[14%] w-full -mt-[6%] justify-center items-center cursor-pointer">
+            <div className=" flex h-[14%] w-full -mt-[2%] justify-center items-center cursor-pointer">
               <h1
                 onMouseEnter={() => setHoveredDiv(1)}
-                onMouseLeave={() => setHoveredDiv(null)}
-                className=" text-white text-xl pt-2 h-[14%]  px-4 py-9 z-50 bg-[#8025F3] rounded-2xl font-optima font-normal ml-1.5"
+                className=" text-white text-xl relative -top-[7%] pt-2 h-[14%]  px-4 py-9 z-50 bg-[#6b6a6a] rounded-2xl font-optima font-normal ml-1.5"
               >
                 A.S Moloobhoy Pvt Ltd
               </h1>
               <p className="target"></p>
             </div>
 
-            <div className="position-2 w-[89%]  h-[14%]  mx-auto text-white flex  cursor-pointer">
+            <div className=" w-[102%]  h-[14%]  mx-auto text-white flex  cursor-pointer">
               {/* <Link to={"/about"}> */}
-                <h1
-                  onMouseEnter={() => setHoveredDiv(2)}
-                  onMouseLeave={() => setHoveredDiv(null)}
-                  className=" text-xl top-[60%]  rounded-2xl h-[14%]  pt-2 px-4 py-9  bg-[#8025F3]  font-optima font-normal"
-                >
-                  A.S Moloobhoy FZCO
-                </h1>
+              <h1
+                onMouseEnter={() => setHoveredDiv(2)}
+                className=" text-xl relative top-[100%] -left-[12%]  rounded-2xl h-[14%]  pt-2 px-4 py-9  bg-[#6b6a6a]  font-optima font-normal"
+              >
+                A.S Moloobhoy FZCO
+              </h1>
               {/* </Link> */}
             </div>
 
-            <div className="w-[113%]  h-[14%] flex justify-end text-white  cursor-pointer">
+            <div className="w-[140%] mt-[6%] h-[14%] flex justify-end text-white  cursor-pointer">
               <h1
                 onMouseEnter={() => setHoveredDiv(3)}
-                onMouseLeave={() => setHoveredDiv(null)}
-                className="ml-1.5 font-optima  pt-2 px-4 py-9 h-[14%] bg-[#8025F3]  rounded-2xl text-xl font-normal"
+                className="ml-1.5 font-optima  relative   pt-2 px-4 py-9 h-[14%] bg-[#6b6a6a]  rounded-2xl text-xl font-normal"
               >
                 A.S Moloobhoy & Sons Pvt Ltd
               </h1>
             </div>
-            <div className="w-[100%] -mr-[10%] h-[14%] text-white flex justify-start items-start cursor-pointer">
+            <div className="w-[100%] relative -left-[32%] -mr-[10%] h-[14%] text-white flex justify-start items-start cursor-pointer">
               <h1
                 onMouseEnter={() => setHoveredDiv(4)}
-                onMouseLeave={() => setHoveredDiv(null)}
-                className="  pt-2 px-4 py-9 h-[14%]  bg-[#8025F3] z-30 rounded-2xl font-optima text-xl font-normal text-white"
+                className="  pt-2 px-4 py-9 h-[14%]  bg-[#6b6a6a] z-30 rounded-2xl font-optima text-xl font-normal text-white"
               >
-                EMME
+                Molobhoy Marine Services LLC
               </h1>
               <li></li>
             </div>
 
-            <div className="h-[14%] w-[110%]  -mt-[7%]  flex justify-end items-end  text-white  cursor-pointer">
+            <div className="h-[14%] relative w-[110%] left-[42%]   flex justify-end items-end  text-white  cursor-pointer">
               <h1
                 onMouseEnter={() => setHoveredDiv(5)}
-                onMouseLeave={() => setHoveredDiv(null)}
-                className=" pt-2 h-11 px-4 bg-[#8025F3]  rounded-2xl font-optima  text-xl  relative font-normal text-white"
+                className=" pt-2 h-11 px-4 bg-[#6b6a6a]  rounded-2xl font-optima  text-xl  relative font-normal text-white"
               >
-                Molobhoy Marine Services LLC
+                Moloobhoy Marine Services WLL:Qatar
               </h1>
             </div>
 
-            <div className=" flex w-[100%]  h-[14%] justify-center items-center cursor-pointer">
+            <div className="relative flex w-[100%] top-[7%]  h-[14%] right-[30%] cursor-pointer">
               <h1
                 onMouseEnter={() => setHoveredDiv(6)}
-                onMouseLeave={() => setHoveredDiv(null)}
-                className=" text-white text-xl pt-2 h-11 px-4 z-50 bg-[#8025F3] rounded-2xl font-optima font-normal ml-1.5"
+                className=" text-white text-xl pt-2 h-11 px-4 z-50 bg-[#6b6a6a] rounded-2xl font-optima font-normal ml-1.5"
               >
-                A.S Moloobhoy Pvt Ltd
+                Moloobhoy Marine Equipment LLP
               </h1>
               <p className="target"></p>
             </div>
 
-            <div className=" flex w-[100%] h-[14%] justify-center items-center cursor-pointer">
+            <div className=" flex w-[100%] relative top-[17%] left-[20%] h-[14%] justify-center items-center cursor-pointer">
               <h1
                 onMouseEnter={() => setHoveredDiv(7)}
-                onMouseLeave={() => setHoveredDiv(null)}
-                className=" text-white text-xl pt-2 h-11 px-4 z-50 bg-[#8025F3] rounded-2xl font-optima font-normal ml-1.5"
+                className=" text-white text-xl pt-2 h-11 px-4 z-50 bg-[#6b6a6a] rounded-2xl font-optima font-normal ml-1.5"
               >
-                A.S Moloobhoy Pvt Ltd
+                Moloobhoy Marine Services LLC-Oman
               </h1>
               <p className="target"></p>
             </div>
@@ -190,7 +247,7 @@ const LandingPage = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
--- src/components/NumberDisplay.jsx
+++ src/components/NumberDisplay.jsx
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from "react";
 import background from "../assets/backgroundimage2.jpg";
 import TextSmokeAnimation from "./TextSmokeAnimation";
 import LandingPage from "./LandingPage";
+// import backgroundVideo from "../assets/""
+import LogoAll from "../assets/logoAll.png";
+import videoBackground from "../assets/bgWaveVideo.mp4";
+import "./numberdisplay.css"
 
 const NumberDisplay = () => {
   const [year, setYear] = useState(1905);
   const [showCountdown, setShowCountdown] = useState(true);
   const [startAnimationPart, setAnimationPart] = useState(false);
-  const [nextPage, setNextPage] = useState(true);
+  const [nextPage, setNextPage] = useState(false);
 
   useEffect(() => {
     const countdownTimeout = setTimeout(() => {
@@ -32,7 +36,7 @@ const NumberDisplay = () => {
     if (year > 2023) {
       const smokeTimeout = setTimeout(() => {
         setShowCountdown(false);
-      }, 700); // Delay for 0.7 seconds after countdown finishes
+      }, 1000); // Delay for 0.7 seconds after countdown finishes
 
       return () => clearTimeout(smokeTimeout);
     }
@@ -59,31 +63,126 @@ const NumberDisplay = () => {
     }
   }
 
+  const skipVideHandler = () => {
+    if (!nextPage) {
+      setNextPage(true);
+    }
+  };
+
   return (
     <>
-    {
-        !nextPage ? (
-            <div
-            className="relative w-full h-screen z-10"
-            style={{
-              backgroundImage: `url(${background})`,
-              backgroundSize: "cover",
-            }}
+      {!nextPage ? (
+        <>
+          <video
+            id="background-video"
+            className="absolute inset-0 w-full h-full object-cover"
+            autoPlay
+            loop
+            muted
           >
-            {startAnimationPart &&
-              (showCountdown ? (
-                <div className="absolute inset-0 flex justify-center items-center">
-                  <h1 className="text-black text-6xl z-20">{year}</h1>
-                </div>
-              ) : (
-                <TextSmokeAnimation setNextPage={setNextPage} />
-              ))}
+            <source src={videoBackground} type="video/mp4" />
+          </video>
+
+          <div
+            className="relative w-full h-screen z-10 overflow-hidden"
+            onClick={() => skipVideHandler()}
+          >
+            <>
+              {startAnimationPart &&
+                (showCountdown ? (
+                  <>
+                    <div className="absolute left-[3%] top-[3%]">
+                      <img src={LogoAll} alt="" />
+                    </div>
+                    <div className="absolute inset-0 flex justify-center items-center">
+                      <svg
+                        version="1.1"
+                        xmlns="http://www.w3.org/2000/svg"
+                        xmlnsXlink="http://www.w3.org/1999/xlink"
+                        x="0px"
+                        y="0px"
+                        width="450px"
+                        height="240px"
+                        xmlSpace="preserve"
+                        className="svg"
+                      >
+                        <defs>
+                          <pattern
+                            id="water"
+                            width=".25"
+                            height="1.1"
+                            patternContentUnits="objectBoundingBox"
+                          >
+                            <path
+                              fill="#000"
+                              d="M0.25,1H0c0,0,0-0.659,0-0.916c0.083-0.303,0.158,0.334,0.25,0C0.25,0.327,0.25,1,0.25,1z"
+                            />
+                          </pattern>
+
+                          <text
+                            id="text"
+                            transform="translate(2,116)"
+                            fontFamily="'Cabin Condensed'"
+                            fontSize="161.047"
+                          >
+                            {year}
+                          </text>
+
+                          <mask id="text-mask">
+                            <use
+                              x="0"
+                              y="0"
+                              xlinkHref="#text"
+                              opacity="1"
+                              fill="#ffffff"
+                            />
+                          </mask>
+
+                          <g id="eff">
+                            <use x="0" y="0" xlinkHref="#text" fill="#a2a3a5" />
+
+                            <rect
+                              className="water-fill"
+                              mask="url(#text-mask)"
+                              fill="url(#water)"
+                              x="-300"
+                              y="50"
+                              width="1200"
+                              height="120"
+                              opacity="0.3"
+                              style={{ animationDuration: "2s" }} // Style as an object
+                            >
+                              <animate
+                                attributeType="xml"
+                                attributeName="x"
+                                from="-300"
+                                to="0"
+                                repeatCount="indefinite"
+                                dur="2s"
+                              />
+                            </rect>
+
+                            {/* Other rect elements with animate tags */}
+                          </g>
+                        </defs>
+
+                        <use
+                          xlinkHref="#eff"
+                          opacity="0.9"
+                          style={{ mixBlendMode: "color-burn" }} // Style as an object
+                        />
+                      </svg>  
+                    </div>
+                  </>
+                ) : (
+                  <TextSmokeAnimation setNextPage={setNextPage} />
+                ))}
+            </>
           </div>
-        ) : (
-            <LandingPage/>
-        )
-    }
-     
+        </>
+      ) : (
+        <LandingPage />
+      )}
     </>
   );
 };
